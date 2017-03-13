59daa5fa1d4810eb545a67082812f576e5a83ba5
florian HENRY <florian.henry@atm-consulting.fr>
2017-03-13 14:26:46 +0100

    Fix comptatiblity for 4.0

--- core/modules/modSingleLogin.class.php
+++ core/modules/modSingleLogin.class.php
@@ -47,7 +47,7 @@ class modSingleLogin extends DolibarrModules
 		$this->family = "other";
 		$this->name = preg_replace('/^mod/i', '', get_class($this));
 		$this->description = "Unique connexion per login";
-		$this->version = '1.8';
+		$this->version = '1.9';
 		$this->const_name = 'MAIN_MODULE_' . strtoupper($this->name);
 		$this->special = 3;
 		$this->picto = 'singlelogin@singlelogin'; 
